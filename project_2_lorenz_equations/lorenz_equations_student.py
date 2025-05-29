0af019f400a26a7e89b26705217ba72709cfb854
zhusiyuv <1477117691@qq.com>
2025-05-29 23:51:11 +0800

    Update lorenz_equations_student.py

--- project_2_lorenz_equations/lorenz_equations_student.py
+++ project_2_lorenz_equations/lorenz_equations_student.py
@@ -23,7 +23,11 @@ def lorenz_system(state, sigma, r, b):
     """
     # TODO: 实现洛伦兹系统方程 (约3行代码)
     # [STUDENT_CODE_HERE]
-    raise NotImplementedError("请实现此函数")
+    x, y, z = state
+    dxdt = sigma * (y - x)
+    dydt = x * (r - z) - y     
+    dzdt = x * y - b * z
+    return [dxdt, dydt, dzdt]
 
 
 def solve_lorenz_equations(sigma=10.0, r=28.0, b=8/3,
@@ -38,7 +42,9 @@ def solve_lorenz_equations(sigma=10.0, r=28.0, b=8/3,
     """
     # TODO: 使用solve_ivp求解洛伦兹方程 (约3行代码)
     # [STUDENT_CODE_HERE]
-    raise NotImplementedError("请实现此函数")
+    t_eval = np.arange(t_span[0], t_span[1], dt)
+    sol = solve_ivp(lorenz_system, t_span, [x0, y0, z0], args=(sigma, r, b), method='RK45', t_eval=t_eval)
+    return sol.t, sol.y
 
 
 def plot_lorenz_attractor(t: np.ndarray, y: np.ndarray):
@@ -47,7 +53,15 @@ def plot_lorenz_attractor(t: np.ndarray, y: np.ndarray):
     """
     # TODO: 实现3D绘图 (约6行代码)
     # [STUDENT_CODE_HERE]
-    raise NotImplementedError("请实现此函数")
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(y[0], y[1], y[2], lw=0.5)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('Lorenz Attractor')
+    plt.savefig('lorenz_attractor.png')  # 保存PNG图像
+    plt.show()
 
 
 def compare_initial_conditions(ic1, ic2, t_span=(0, 50), dt=0.01):
@@ -56,8 +70,20 @@ def compare_initial_conditions(ic1, ic2, t_span=(0, 50), dt=0.01):
     """
     # TODO: 实现初始条件比较 (约10行代码)
     # [STUDENT_CODE_HERE]
-    raise NotImplementedError("请实现此函数")
+    t, y1 = solve_lorenz_equations(x0=ic1[0], y0=ic1[1], z0=ic1[2], dt=dt)
+    _, y2 = solve_lorenz_equations(x0=ic2[0], y0=ic2[1], z0=ic2[2], dt=dt)
+    plt.figure(figsize=(10, 6))
+    plt.plot(t, y1[0], label='x(t) IC1')
+    plt.plot(t, y2[0], label='x(t) IC2', linestyle='--')
+    plt.xlabel('Time')
+    plt.ylabel('x(t)')
+    plt.title('Comparison of Initial Conditions')
+    plt.legend()
 
+    plt.savefig('initial_conditions_comparison.png') 
+    plt.show()
+    
+    
 
 def main():
     """
